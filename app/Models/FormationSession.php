e9b86e4e325ab90b65b1ee3704d8c8646ed02d17
Elis0u <dadure.elisa@outlook.com>
2026-08-08 15:40:41 +0200

    feat: add registrations() relation on FormationSession

--- app/Models/FormationSession.php
+++ app/Models/FormationSession.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FormationSession extends Model
 {
@@ -19,5 +20,10 @@ class FormationSession extends Model
     public function trainer()
     {
         return $this->belongsTo(User::class, 'trainer_id');
+    }
+
+    public function registrations(): HasMany
+    {
+        return $this->hasMany(Registration::class, 'session_id');
     }
 }
